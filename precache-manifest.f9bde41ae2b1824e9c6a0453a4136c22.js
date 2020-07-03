e42e67d50c1316fbe55428bf56933089e0798405
Travis Turner <RoboEnigma@gmail.com>
2020-07-03 19:02:44 +0300

    Updates

---
+++ precache-manifest.f9bde41ae2b1824e9c6a0453a4136c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bceca464211416312d1a384521aa15",
+    "url": "/emperor-mattress-ru/index.html"
+  },
+  {
+    "revision": "7fdb1868c67d50419df9",
+    "url": "/emperor-mattress-ru/static/css/main.788298e6.chunk.css"
+  },
+  {
+    "revision": "78e88ee31c5cb2b713e5",
+    "url": "/emperor-mattress-ru/static/js/2.39cf9988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emperor-mattress-ru/static/js/2.39cf9988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdb1868c67d50419df9",
+    "url": "/emperor-mattress-ru/static/js/main.2eeb6ca9.chunk.js"
+  },
+  {
+    "revision": "5423e1b3e5cce39ec02a",
+    "url": "/emperor-mattress-ru/static/js/runtime-main.3be614bf.js"
+  },
+  {
+    "revision": "4f176ca99b0e03acb83dacc52370874e",
+    "url": "/emperor-mattress-ru/static/media/pacifico-regular.4f176ca9.woff"
+  }
+]);
