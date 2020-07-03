34ccb568638e2512a375eacedc781304b76157a5
Travis Turner <RoboEnigma@gmail.com>
2020-07-03 18:39:22 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './landing/Header';
+import Bed from './landing/Bed';
+import InfoForm from './landing/InfoForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Bed />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
+
+      <InfoForm />
+
+      */
